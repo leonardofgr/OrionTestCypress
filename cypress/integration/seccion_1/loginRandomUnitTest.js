e9481811e34989cd8467db8ccdee7a105f3e7b0e
Leonardo Figueroa <44540072+FgrDesarrollo@users.noreply.github.com>
2021-09-29 18:28:03 -0300

    se actualiza version de cypres 8.5.0

--- cypress/integration/seccion_1/loginRandomUnitTest.js
+++ cypress/integration/seccion_1/loginRandomUnitTest.js
@@ -8,7 +8,7 @@ describe('Login Tests with random data', () => {
     })
     datos.forEach(test => {
         
-        it('Se espera no ingresar a Orion por credenciales incorrectas',()=>{
+        it('Se espera no poder ingresar a Orion por uso de credenciales incorrectas',()=>{
             cy.get('#email').type(test.email)
             cy.get('#password').type(test.passwoord)
             cy.xpath("//body[1]/div[1]/main[1]/div[1]/div[1]/div[1]/div[1]/div[1]/form[1]/div[4]/div[1]/button[1]").click()
